209dc7121e106d1648a2321d77dea11ea3b3b31e
Hyugasi <isaiah.gordon@gmail.com>
2020-06-03 10:48:14 -0500

    made the routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Redirect, Link } from 'react-router-dom'
 import './App.css';
+import Home from './components/home/home'
+import Projects from './components/projects/projects'
+import Contact from './components/contact/contact'
+import About from './components/about/about'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/">
+          Home
+        </Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
       </header>
+      <body>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </body>
     </div>
   );
 }
