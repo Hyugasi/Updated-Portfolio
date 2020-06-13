7b2ec5a9089ec60820e46589359fa4e142bc5443
Hyugasi <isaiah.gordon@gmail.com>
2020-06-13 14:34:36 -0500

    got the projects

--- portfolio/src/components/projects/projects.js
+++ portfolio/src/components/projects/projects.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Projects() {
+    const [projectsData, setProjectsData] = useState([])
+
+    useEffect(() => {
+        const id = `14G7FymaxpdAgNrQGA0VjtZpni3n6LBznLBRQ9fuUbrc`
+        const projectsUrl = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+        const makeAPICall = async () => {
+            const res = await fetch(projectsUrl)
+            const json = await res.json()
+            setProjectsData(json)
+            console.log(json)
+        }
+        makeAPICall()
+    }, [])
+
+    // const mappedProjects = projectsData.
     return (
         <div>
             Projects
